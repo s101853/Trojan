e4d6d7a3177ac4299b1f11dc49751ec4f372c371
s101853 <s101853@ap.be>
2024-04-12 08:37:44 +0200

    Update take_screenshot_module.py

--- take_screenshot_module.py
+++ take_screenshot_module.py
@@ -2,13 +2,9 @@ from PIL import ImageGrab
 
 def take_screenshot():
     try:
-        # Capture the screenshot
         screenshot = ImageGrab.grab()
-
-        # Save the screenshot
         screenshot.save("screenshot.png")
 
-        print("Screenshot captured successfully.")
     except Exception as e:
         print(f"Failed to capture screenshot: {str(e)}")
 
