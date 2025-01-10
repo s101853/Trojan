fb54cc04b9b46525df37816a8c5570644cc7dcb0
s101853 <s101853@ap.be>
2025-01-10 10:04:14 +0100

    Update take_screenshot_module.py

--- take_screenshot_module.py
+++ take_screenshot_module.py
@@ -1,29 +1,28 @@
 import platform
 import os
-from PIL import ImageGrab 
-import pyautogui 
+from PIL import ImageGrab
+import pyautogui
 
 def take_screenshot():
     try:
         system_name = platform.system()
 
-        if system_name == "Windows" or system_name == "Darwin": 
-            # Use ImageGrab for Windows and macOS
+        if system_name == "Windows" or system_name == "Darwin":
             screenshot = ImageGrab.grab()
             screenshot.save("screenshot.png")
             print("Screenshot saved as screenshot.png")
 
         elif system_name == "Linux":
-            def take_screenshot():
-                try:
-                    screenshot = pyautogui.screenshot()
-                    screenshot.save("screenshot2.png")
-                    print("Screenshot saved as screenshot2.png")
-                except Exception as e:
-                    print(f"Failed to capture screenshot: {str(e)}")
-            
-            take_screenshot()
-
+            os.system("xhost +")
+            try:
+                screenshot = pyautogui.screenshot()
+                screenshot.save("screenshot2.png")
+                print("Screenshot saved as screenshot2.png")
+            except Exception as e:
+                print(f"Failed to capture screenshot: {str(e)}")
+            finally:
+                os.system("xhost -")
+                
         else:
             print(f"Unsupported platform: {system_name}")
 
